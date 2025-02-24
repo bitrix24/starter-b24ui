89499eb6c96af4e5eb521f4c5954df6fc86fb78f
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-02-24 06:22:50 +0300

    chore(deps): update

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,36 +4,27 @@ import PlayCircleIcon from '@bitrix24/b24icons-vue/main/PlayCircleIcon'
 
 <template>
   <div class="flex flex-col items-center justify-center gap-16 h-screen">
-    <h1 class="font-b24-secondary text-h1 sm:text-8xl font-light">
-      Bitrix24 UI - Playground
-    </h1>
-    
+  <h1 class="font-b24-secondary text-h1 sm:text-8xl font-light">
+    Bitrix24 UI - Playground
+  </h1>
     <div class="flex flex-wrap items-center gap-2">
-      <B24Button
-        rounded
-        label="Documentation"
-        color="ai"
-        :icon="PlayCircleIcon"
-        to="https://bitrix24.github.io/b24ui/guide/installation-nuxt-app.html"
-        target="_blank"
-      />
-  
-      <B24Button
-        rounded
-        label="GitHub"
-        color="link"
-        depth="dark"
-        to="https://github.com/bitrix24/b24ui"
-        target="_blank"
-      />
-    </div>
-    
     <B24Button
       rounded
-      label="Main Page"
+      label="Documentation"
+      color="ai"
+      :icon="PlayCircleIcon"
+      to="https://bitrix24.github.io/b24ui/guide/installation-nuxt-app.html"
+      target="_blank"
+    />
+
+    <B24Button
+      rounded
+      label="GitHub"
       color="link"
       depth="dark"
-      to="/main"
+      to="https://github.com/bitrix24/b24ui"
+      target="_blank"
     />
+    </div>
   </div>
 </template>
