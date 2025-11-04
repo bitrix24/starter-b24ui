1cc2759a57520088ba3c58a2cdea18d377b7182f
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-11-04 16:06:05 +0300

    chore(deps): update

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,3 +1,6 @@
 export default defineAppConfig({
-  // https://bitrix24.github.io/b24ui/guide/getting-started.html
+  // https://bitrix24.github.io/b24ui/docs/getting-started/installation/nuxt/
+  // experimental: {
+  //   componentDetection: true
+  // }
 })
