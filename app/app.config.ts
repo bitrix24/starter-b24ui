48b047dcc902018e9e37cdaec397652ab2c27e5b
Shevchik Igor <s.hevchik.mail@gmail.com>
2026-03-02 17:53:40 +0300

    chore(deps): update

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,5 +1,6 @@
 export default defineAppConfig({
   b24ui: {},
   colorMode: true,
-  colorModeTypeLight: 'light' as const
+  colorModeInitialValue: 'light' as const, // edge-dark | edge-light | light
+  colorModeTypeLight: 'light' as const // edge-dark | edge-light | light
 })
--- app/app.vue
+++ app/app.vue
@@ -12,7 +12,7 @@ useHead({
   ],
   htmlAttrs: {
     lang: 'en',
-    class: 'light ddd-2'
+    class: 'light'
   }
 })
 
@@ -37,63 +37,60 @@ const tgLink = computed(() => {
 
 <template>
   <B24App>
-    <B24DashboardGroup>
-      <B24SidebarLayout
-        :use-light-content="false"
-        :b24ui="{
-          headerWrapper: 'max-w-(--ui-container) mx-auto',
-          containerWrapperInner: 'size-auto max-w-(--ui-container) mx-auto'
-        }"
-      >
-        <template #navbar>
-          <B24NavbarSection class="inline-flex">
-            <RouterLink to="/">
-              <AppLogo class="w-auto h-[40px] shrink-0" />
-            </RouterLink>
-          </B24NavbarSection>
+    <B24Header>
+      <template #left>
+        <NuxtLink to="/">
+          <AppLogo class="w-auto h-[40px] shrink-0" />
+        </NuxtLink>
 
-          <B24NavbarSection>
-            <TemplateMenu />
-          </B24NavbarSection>
+        <TemplateMenu />
+      </template>
 
-          <B24NavbarSpacer />
-
-          <B24NavbarSection class="gap-1 sm:gap-3">
-            <B24ColorModeButton :content="{ align: 'end', side: 'bottom' }" />
-            <B24Button
-              :to="tgLink"
-              target="_blank"
-              aria-label="Telegram"
-              color="air-tertiary-no-accent"
-              :icon="TelegramIcon"
-              size="sm"
-            />
-            <B24Button
-              to="https://github.com/bitrix24/starter-b24ui"
-              target="_blank"
-              aria-label="GitHub"
-              color="air-tertiary-no-accent"
-              :icon="GitHubIcon"
-              size="sm"
-            />
-          </B24NavbarSection>
-        </template>
-
-        <NuxtPage />
-
-        <template #content-bottom>
-          <B24Separator :icon="Bitrix24Icon" />
-          <div class="min-w-0 flex-1 max-w-(--ui-container) px-(--content-area-shift) mx-auto mb-4 flex flex-row flex-wrap items-center justify-between gap-4">
-            <ProseP
-              small
+      <template #right>
+        <B24ColorModeButton :content="{ align: 'end', side: 'bottom' }" />
+        <ClientOnly>
+          <B24Button
+            :to="tgLink"
+            target="_blank"
+            aria-label="Telegram"
+            color="air-tertiary-no-accent"
+            :icon="TelegramIcon"
+            size="sm"
+          />
+          <template #fallback>
+            <B24Skeleton
               accent="less"
-              class="mb-0"
-            >
-              Built with Bitrix24 UI • &copy; {{ new Date().getFullYear() }}
-            </ProseP>
-          </div>
-        </template>
-      </B24SidebarLayout>
-    </B24DashboardGroup>
+              class="size-7 rounded-[8px]"
+            />
+          </template>
+        </ClientOnly>
+        <B24Button
+          to="https://github.com/bitrix24/starter-b24ui"
+          target="_blank"
+          aria-label="GitHub"
+          color="air-tertiary-no-accent"
+          :icon="GitHubIcon"
+          size="sm"
+        />
+      </template>
+    </B24Header>
+
+    <B24Main>
+      <NuxtPage />
+    </B24Main>
+
+    <B24Separator :icon="Bitrix24Icon" />
+
+    <B24Footer>
+      <template #left>
+        <ProseP
+          small
+          accent="less"
+          class="mb-0"
+        >
+          Built with Bitrix24 UI • &copy; {{ new Date().getFullYear() }}
+        </ProseP>
+      </template>
+    </B24Footer>
   </B24App>
 </template>
